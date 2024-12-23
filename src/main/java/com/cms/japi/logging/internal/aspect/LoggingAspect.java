c4157b9100e47710f5d154e80e0f00f543295844
Milan Jovanovic <milanj1705@gmail.com>
2024-12-23 17:44:42 +0100

    Implement exception logging

--- src/main/java/com/cms/japi/logging/internal/aspect/LoggingAspect.java
+++ src/main/java/com/cms/japi/logging/internal/aspect/LoggingAspect.java
@@ -1,9 +1,12 @@
 package com.cms.japi.logging.internal.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -19,6 +22,14 @@ public class LoggingAspect {
     @Around("@annotation(com.cms.japi.logging.LogService)")
     public Object logAnnotatedMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         return performLogging(joinPoint);
+    }
+
+    @After("@annotation(org.springframework.web.bind.annotation.ExceptionHandler)")
+    public void logExceptionHandling(JoinPoint joinPoint) {
+        String handlerName = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        if(args.length == 0) return;
+        log.info("Exception {} handled with handler {}", args[0].getClass().getName(), handlerName);
     }
 
     private Object performLogging(ProceedingJoinPoint joinPoint) throws Throwable {
