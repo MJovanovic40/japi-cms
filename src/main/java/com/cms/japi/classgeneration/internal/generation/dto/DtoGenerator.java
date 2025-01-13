d571f2929f0e674fbd3ff0b8dcead10b4a063633
Milan Jovanovic <milanj1705@gmail.com>
2025-01-13 22:23:34 +0100

    Implement Dto Generator

--- src/main/java/com/cms/japi/classgeneration/internal/generation/dto/DtoGenerator.java
+++ src/main/java/com/cms/japi/classgeneration/internal/generation/dto/DtoGenerator.java
@@ -1,36 +1,31 @@
-package com.cms.japi.classgeneration.internal.generation.model;
+package com.cms.japi.classgeneration.internal.generation.dto;
 
 import com.cms.japi.JapiApplication;
+import com.cms.japi.classgeneration.internal.generation.ClassGenerator;
 import com.cms.japi.commons.dynamicclassproperties.DynamicClassField;
 import com.cms.japi.commons.dynamicclassproperties.DynamicClassProperties;
-import com.cms.japi.classgeneration.internal.generation.ClassGenerator;
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 
-@RequiredArgsConstructor
-public class ModelGenerator implements ClassGenerator {
+public class DtoGenerator implements ClassGenerator {
 
     @Override
     public Class<?> generate(DynamicClassProperties dynamicClassProperties) {
-        var builder = new ByteBuddy().subclass(GeneratedEntity.class)
-                .annotateType(AnnotationDescription.Builder.ofType(Entity.class).build());
+        var builder = new ByteBuddy().subclass(GeneratedDto.class)
+                .annotateType(AnnotationDescription.Builder.ofType(Data.class).build())
+                .annotateType(AnnotationDescription.Builder.ofType(NoArgsConstructor.class).build());
 
         for (DynamicClassField field : dynamicClassProperties.getFields()) {
-            builder = builder.defineField(dynamicClassProperties.getName() + "_" + field.getName(), String.class)
+            builder = builder.defineField(field.getName(), String.class)
                     .annotateField(AnnotationDescription.Builder.ofType(Column.class).build());
         }
 
-        builder = builder.defineField("id", Long.class).annotateField(
-                AnnotationDescription.Builder.ofType(Id.class).build(),
-                AnnotationDescription.Builder.ofType(Column.class).build(),
-                AnnotationDescription.Builder.ofType(GeneratedValue.class).build());
+        builder = builder.defineField("id", Long.class);
 
         DynamicType.Unloaded<?> generatedClass = builder.name(dynamicClassProperties.getName()).make();
         return generatedClass.load(JapiApplication.class.getClassLoader(), ClassLoadingStrategy.Default.INJECTION)
